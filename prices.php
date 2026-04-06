9c0474d4fc1bfbd5a84f1da02aaa4ab3a4c1527a
sample <sample>
2026-04-06 22:30:57 +0900

    commit

--- prices.php
+++ prices.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Beauty Layout</title>
+  <title>Beauty#</title>
   <link rel="stylesheet" href="css/sample_common.css">
   <script src="js/sample_common.js" defer></script>
   <style>
